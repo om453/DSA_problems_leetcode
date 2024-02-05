7b8ce8cbc1659716be845429ac722d791977605f
Om453 <om.patel.cs21@ggits.net>
2024-02-05 21:26:11 +0530

    initial commit

---
+++ Basics of programming/Statements.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //? If else statement
+    // int age;
+    // cin>>age;
+    //   if (age <= 18)
+    //   {
+    //     cout<<"adult"<<endl;
+    //   }
+    //   else
+    //   {
+    //     cout<<"not adult";
+    //   }
+
+//? Else-if statement 
+// int age;
+// cin>>age;
+
+// if (age<18)
+// {
+//     cout<<"not eligible for job"<<endl;
+// }
+// else if (age<=54)  //also but no need to write like this age>=18 && age<=54
+// {
+//     cout<<"eligible for job"<<endl;
+// }
+// else if ( age<=54)
+// {
+//     cout<<"not eligible for job, nut not retirement"<<endl;
+// }
+// else
+// {
+//     cout<<"retirement time";
+// }
+
+
+// ? Switch case
+// int day = 4;
+
+//     switch (day) {
+//         case 1:
+//             cout << "Monday";
+//             break;
+//         case 2:
+//             cout << "Tuesday";
+//             break;
+//         case 3:
+//             cout << "Wednesday";
+//             break;
+//         case 4:
+//             cout << "Thursday";
+//             break;
+//         case 5:
+//             cout << "Friday";
+//             break;
+//         case 6:
+//             cout << "Saturday";
+//             break;
+//         case 7:
+//             cout << "Sunday";
+//             break;
+//         default:
+//             cout << "Invalid";
+//     }
+
+
+
+      
+    return 0;
+}
---
+++ Basics of programming/Variable.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h> // It will include all the libraries of C++
+using namespace std;
+
+int main()
+{
+    // cout<<"Hello DSA";
+
+// ? Datatypes int , long ,long long, float, double, string , char
+
+
+
+    // int a = 10;
+    // float b = 12.1;
+    // double c = 1213.2;
+    // cout<<a<<"\n"<<b<<"\n"<<c;
+
+
+    // string s1;
+    // string s2;
+    // cin>>s1>>s2;
+    // cout<<s1<<" "<<s2;
+
+    // * If we want to take whole string with gap as well we can use getline()
+
+    // string str;
+    // getline(cin , str);
+    // cout<< str;
+    // return 0;
+
+
+    char ch;
+    cin>>ch;
+    cout<<ch;
+    
+}
