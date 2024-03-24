fdc36ef1951a1d4046de3ea1664981b1a548b99d
Om Patel <116504580+om453@users.noreply.github.com>
2024-03-24 22:32:41 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (88.91%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        
+        
+// By using 2 pointer approach
+        reverse(s.begin(), s.end());
+        int i = 0;
+        int l = 0, r = 0;
+        
+        int n = s.length();
+        
+        while(i < n) {
+            while(i < n && s[i] != ' ') { //i ko agar char dikha to r ko dega and i++ and r++
+                s[r++] = s[i++];
+            }
+            
+            if(l < r) {
+                reverse(s.begin()+l, s.begin()+r);
+                
+                s[r] = ' ';
+                r++;
+                
+                l = r;
+            }
+            
+            i++; 
+        }
+        
+        s = s.substr(0 , r-1);
+        
+        return s; 
+    }
+};
