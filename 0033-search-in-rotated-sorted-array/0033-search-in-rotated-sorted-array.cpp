4e0a34ce7f476b055ab2611c93cd3a46fbc5df80
Om Patel <116504580+om453@users.noreply.github.com>
2024-03-29 23:28:05 +0530

    Time: 3 ms (61.24%), Space: 13.5 MB (77.77%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        // Using 2 pointers approach
+
+        int n = nums.size(); 
+        int low = 0;
+        int high = n - 1;
+
+        while(low <= high){
+            int mid = (low + high) / 2;
+
+        if (nums[mid] == target) return mid;
+
+       //if left part is sorted
+        if (nums[low] <= nums[mid]) {
+            if (nums[low] <= target && target <= nums[mid]) {
+                //element exists
+                high = mid - 1;
+            }
+            else {
+                //element does not exist
+                low = mid + 1;
+            }
+        }
+        else { //if right part is sorted
+            if (nums[mid] <= target && target <= nums[high]) {
+                //element exists
+                low = mid + 1;
+            }
+            else {
+                //element does not exist
+                high = mid - 1;
+            }
+        }
+    }
+    return -1;
+
+   }
+};
