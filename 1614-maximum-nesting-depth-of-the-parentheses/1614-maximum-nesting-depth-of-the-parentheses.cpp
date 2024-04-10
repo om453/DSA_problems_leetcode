14f59545b47247d93bb34b9c701d60019948f4db
Om Patel <116504580+om453@users.noreply.github.com>
2024-04-10 10:37:04 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (21.69%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+       int openBrackets = 0;
+       int result = 0;
+
+       for(char &ch : s){       // range-based looping
+        if(ch == '('){
+            openBrackets++;
+        }
+        else if(ch == ')'){
+            openBrackets--;
+        }
+
+        result = max(result , openBrackets);
+       } 
+
+       return result;
+    }
+};
