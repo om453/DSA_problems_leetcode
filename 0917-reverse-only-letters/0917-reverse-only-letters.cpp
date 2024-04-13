5826ab4e0b5f3cbb24837446fdeb68a85fbbb63c
Om Patel <116504580+om453@users.noreply.github.com>
2024-04-13 10:21:04 +0530

    Time: 3 ms (44.38%), Space: 7.3 MB (66.21%) - LeetHub

---
+++ 0917-reverse-only-letters/0917-reverse-only-letters.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int low = 0;
+        int high = s.length()-1;
+        while(high>=low){
+            if(isalpha(s[low]) && isalpha(s[high])){    //using the isalpha() function for check if both are alphabetic
+                swap(s[low],s[high]);
+                low++;
+                high--;
+            }
+            else if (!isalpha(s[low])){      // If the character at index low is not an alphabetic character
+                low++;
+            }
+            else{                          // If the character at index high is not an alphabetic character
+                high--;
+            }
+        }
+        return s;
+    }
+};
