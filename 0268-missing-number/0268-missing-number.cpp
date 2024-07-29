38a42e751f2d6bad493581f757d44c21fbe5678c
Om Patel <116504580+om453@users.noreply.github.com>
2024-07-29 21:18:50 +0530

    Update 0268-missing-number.cpp

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,3 +1,32 @@
+//* By using hashing method 
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+       // Create an unordered map to store number frequencies
+        unordered_map<int, int> map;
+
+        // Populate the map with number frequencies
+        for (auto i : nums) {
+            map[i]++;
+        }
+
+        // Find the missing number
+        for (int i = 0; i < nums.size(); i++) {
+            if (map.count(i) == 0) {
+                return i;
+            }
+        }
+
+        // If no missing number found, return the largest possible number
+        return nums.size();
+
+    }
+};
+
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//* By using  summation method 
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
