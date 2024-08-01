d4053347960c93664a8ad23515a37f5d89508994
Om Patel <116504580+om453@users.noreply.github.com>
2024-08-01 20:05:09 +0530

    Update 0231-power-of-two.cpp

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -6,3 +6,19 @@ public:
         return (n & (n - 1)) == 0; // Check if n is a power of two
     }
 };
+
+//-----------------------------------------------------------------------------------------------
+
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n==1) return true;
+        if(n%2==1) return false;
+        if(n<1) return false;
+
+       // By simple recursive approach
+        return isPowerOfTwo(n/2);
+
+    }
+};
