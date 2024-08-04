60e11d1b29b9b860996ec9d828e5534d27af2ee7
Om Patel <116504580+om453@users.noreply.github.com>
2024-08-04 21:50:23 +0530

    Time: 11 ms (44.46%), Space: 10.6 MB (55.27%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+    // By using Tortoise and Hare Algorithm  
+
+    ListNode* slow = head;
+    ListNode* fast = head;
+
+    
+    while (fast != nullptr && fast->next != nullptr) {
+        // Move slow one step
+        slow = slow->next;
+        // Move fast two steps
+        fast = fast->next->next;
+
+        // Check if slow and fast pointers meet
+        if (slow == fast) {
+            return true;  // Loop detected
+        }
+    }
+
+    // If fast reaches the end of the list, there is no loop
+    return false;
+    }
+};
