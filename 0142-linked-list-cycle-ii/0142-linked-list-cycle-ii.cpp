cdd2605b6804373990313fc0b3f73c91c19d859f
Om Patel <116504580+om453@users.noreply.github.com>
2024-08-04 23:47:46 +0530

    Time: 6 ms (71.36%), Space: 10.3 MB (44.90%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+               // Initialize a slow and fast 
+    // pointers to the head of the list
+    ListNode* slow = head;  
+    ListNode* fast = head;  
+
+    // Phase 1: Detect the loop
+    while (fast != NULL && fast->next != NULL) {  
+        slow = slow->next;         // Move slow one step
+        fast = fast->next->next;  // Move fast two steps  
+
+        // If slow and fast meet, a loop is detected
+        if (slow == fast) {
+            slow = head; // Reset the slow pointer to the head of the list
+
+            // Phase 2: Find the first node of the loop
+            while (slow != fast) {
+                
+                // Move slow and fast one step at a time
+                slow = slow->next;  
+                fast = fast->next;  
+
+                // When slow and fast meet again,
+                // it's the first node of the loop
+            }
+            
+            // Return the first node of the loop
+            return slow;  
+        }
+
+    }
+   return NULL;
+  }
+};
