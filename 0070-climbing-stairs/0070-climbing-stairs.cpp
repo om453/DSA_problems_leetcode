875b8fc2b9cb7c69d91ae646fd1d261e8b8d752b
Om Patel <116504580+om453@users.noreply.github.com>
2024-08-16 01:01:41 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (45.02%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,20 +2,28 @@ class Solution {
 
     //By Recursion with Memo Approach : Top Down
 public:
+      // Memoization table to store calculated results
     int t[46];
-    int solve(int n){
-        if(n == 0 || n == 1 || n == 2)
-            return n;
-        
-        if(t[n] != -1)
-            return t[n];
-        
-        int a = solve(n-1);
-        int b = solve(n-2);
-        
-        return t[n] = a+b;
-        
+
+    int solve(int n) {
+        // Base cases:
+        if (n == 0 || n == 1 || n == 2) {
+            return n; // Number of ways to climb 0, 1, or 2 steps
+        }
+
+        // Check if the result for n is already calculated:
+        if (t[n] != -1) {
+            return t[n]; // Return the pre-calculated result
+        }
+
+        // Calculate the number of ways to climb n steps:
+        int a = solve(n - 1); 
+        int b = solve(n - 2); 
+
+        // Store the result in the memoization table
+        return t[n] = a + b; // Total ways to climb n steps
     }
+
     int climbStairs(int n) {
         memset(t, -1, sizeof(t));
         return solve(n);
