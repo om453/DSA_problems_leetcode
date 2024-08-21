8762cf887f845b4029db83a5e1bedccab56aed45
Om Patel <116504580+om453@users.noreply.github.com>
2024-08-21 22:24:19 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (34.79%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -1,6 +1,19 @@
+//T.C : O(log(n)) and S.C : O(1)
 class Solution {
 public:
     int numberOfMatches(int n) {
-        return n-1;
+        int result = 0;
+        
+        while(n > 1) {
+            if(n%2 == 0) {
+                result += n/2;
+                n /= 2;
+            } else {
+                result += (n-1)/2;
+                n = ((n-1)/2) + 1;
+            }
+        }
+        
+        return result;
     }
 };
