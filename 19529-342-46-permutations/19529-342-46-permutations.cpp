f6df3cf488b1da666cd1de5ea0f83e8e87e59c0d
Om Patel <116504580+om453@users.noreply.github.com>
2025-01-03 12:19:11 +0530

    Time: 0 ms, Memory: 11.1 MB - LeetHub

---
+++ 19529-342-46-permutations/19529-342-46-permutations.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> result;
+    int n;
+    
+    void solve(int idx, vector<int>& nums) {
+
+        if(idx == n) {
+            result.push_back(nums);
+            return;
+        }
+        
+        for(int i = idx; i < n; i++) {
+            swap(nums[i], nums[idx]);     // Swapping elements in nums
+            
+            solve(idx+1, nums);
+            
+            swap(nums[i], nums[idx]);
+        }
+        
+        
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+
+        n = nums.size();        
+        solve(0, nums);        
+        return result;
+    }
+};
