1902b5c6c01fa7702c4acc667186081700662009
Om Patel <116504580+om453@users.noreply.github.com>
2025-01-05 11:58:43 +0530

    Time: 0 ms, Memory: 19.3 MB - LeetHub

---
+++ 0733-2059-908-smallest-range-i/0733-2059-908-smallest-range-i.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int smallestRangeI(vector<int>& nums, int k) {     
+     int mini=INT_MAX,maxi=INT_MIN;
+        for(int i=0;i<nums.size();i++)
+        {
+            mini=min(mini,nums[i]);
+            maxi=max(maxi,nums[i]);
+        }
+        mini+=k;
+        maxi-=k;
+        return max(0,maxi-mini); //To get a positive value, if it's -ve it should return 0   
+    }
+};
