a72d9f4e6a518e8b0774a48f40d1a956d4273c9a
Om Patel <116504580+om453@users.noreply.github.com>
2025-01-08 23:35:04 +0530

    Time: 0 ms, Memory: 7.9 MB - LeetHub

---
+++ 0556-33-1952-three-divisors/0556-33-1952-three-divisors.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isThree(int n) {
+        int count=0; //for counting divisor of n
+
+        for(int i=1; i<=n && count<4; i++)
+        {
+            if(n%i==0)
+                count++;
+        }
+        if(count==3)
+            return true;
+
+        return false;    
+    }
+};
