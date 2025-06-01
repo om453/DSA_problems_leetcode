263b86b31b30e9901b1eafd3c10f1b17d70ebbc5
Om Patel <116504580+om453@users.noreply.github.com>
2025-06-01 20:48:32 +0530

    Time: 31 ms (26.47%), Space: 8.9 MB (84.71%) - LeetHub

--- 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.cpp
+++ 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.cpp
@@ -3,13 +3,13 @@ public:
     long long distributeCandies(int n, int limit) {
     long long ways = 0;
 
-        //child1
+  
         int minCh1 = max(0, n - 2*limit);
         int maxCh1 = min(n, limit);
 
-        for(int i = minCh1; i <= maxCh1; i++) { //fixing child1 assignment of candies
+        for(int i = minCh1; i <= maxCh1; i++) { 
 
-            int N = n-i; //for ch2 and ch3
+            int N = n-i; 
 
             int minCh2 = max(0, N - limit);
             int maxCh2 = min(N, limit);
